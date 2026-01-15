31b7f1fcf8e6192823b8974d30860e98206f8b5b
Simon Walker <s.r.walker101@googlemail.com>
2026-01-15 17:01:18 +0000

    Conditionally add frameworks depending on os

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 fn main() {
     println!("cargo:rustc-link-search=.");
     println!("cargo:rustc-link-lib=static=tailscale");
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
-    println!("cargo:rustc-link-lib=framework=IOKit");
-    println!("cargo:rustc-link-lib=framework=Security");
+
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-lib=framework=CoreFoundation");
+        println!("cargo:rustc-link-lib=framework=IOKit");
+        println!("cargo:rustc-link-lib=framework=Security");
+    }
 }
